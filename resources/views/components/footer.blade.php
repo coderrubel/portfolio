fc3fe47c57b4ee5b0ead7acfe324fb13c04db94e
coderrubel <95075500+rubel-sebpo@users.noreply.github.com>
2023-09-18 19:12:23 +0600

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,27 @@
+<footer>
+        <div class="container">
+            <div class="social">
+                <a href="#">facebook </a>
+                <a href="#">twitter </a>
+                <a href="#">instagram </a>
+                <a href="#">google plus </a>
+                <a href="#">behance </a>
+                <a href="#">dribbble  </a>
+            </div>
+            <p>Copyright © 2023 Berlin, All rights Reserved. <br/>Created by tavonline</p>
+        </div>
+    </footer>
+
+<!-- Javascripts -->
+<script src="{{ asset('fontend/js/jquery-2.1.4.min.js')}}"></script><!-- jQuery library -->
+<script src="{{ asset('fontend/js/smoothscroll.js')}}"></script>
+<script src="{{ asset('fontend/js/bootstrap.min.js')}}"></script> 
+<script src="{{ asset('fontend/js/wow.min.js')}}"></script>
+<script src="{{ asset('fontend/js/isotope.pkgd.min.js')}}"></script>
+<script src="{{ asset('fontend/js/typed.js')}}"></script>
+<script src="{{ asset('fontend/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{ asset('fontend/js/jquery.superslides.min.js')}}"></script>
+<script src="{{ asset('fontend/js/owl.carousel.min.js')}}"></script>
+<script src="{{ asset('fontend/js/main.js')}}"></script> 
+</body>
+</html>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Berlin - Portfolio</title>
+<meta charset="UTF-8">
+<meta name="description" content="Berlin Portfolio Template">
+<meta name="keywords" content="personal, portfolio">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="{{ asset('fontend/css/bootstrap.css')}}"/>
+<link rel="stylesheet" href="{{ asset('fontend/css/reset.css')}}"/>
+<link rel="stylesheet" href="{{ asset('fontend/css/style.css')}}"/>
+<link rel="stylesheet" href="{{ asset('fontend/css/animate.css')}}"/>
+<link rel="stylesheet" href="{{ asset('fontend/css/owl.carousel.css')}}"/> 
+<link rel="stylesheet" href="{{ asset('fontend/css/magnific-popup.css')}}"/> 
+    
+<!-- Google Web fonts -->
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+<!-- Font icons -->
+<link rel="stylesheet" href="{{ asset('fontend/icon-fonts/font-awesome-4.5.0/css/font-awesome.min.css')}}"/>
+<link rel="stylesheet" href="{{ asset('fontend/icon-fonts/essential-regular-fonts/essential-icons.css')}}"/>
+
+</head>
+<body class="diag">
+    
+<!-- LOADER -->
+<div class="loader-wrapper">
+    <div class="loader"></div>
+</div>
+    
+    <nav>
+        <div class="row">
+            <div class="container">
+                <div class="logo">
+                    <img src="{{ asset('fontend/images/logo.png')}}" alt="">
+                </div>
+                <div class="responsive"><i data-icon="m" class="icon"></i></div>
+                <ul class="nav-menu">
+                    <li><a href="#home" class="smoothScroll">Home</a></li>
+                    <li><a href="#about" class="smoothScroll">About</a></li>
+                    <li><a href="#portfolio" class="smoothScroll">Portfolio</a></li>
+                    <li><a href="#blog" class="smoothScroll">Blog</a></li>
+                    <li><a href="#contact" class="smoothScroll">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
